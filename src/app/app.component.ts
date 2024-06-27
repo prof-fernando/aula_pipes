e559e9c0c1f0cf58efd5ab01ce5198a1c2dad6a2
Fernando Luis <fernando@Fernandos-MacBook-Pro.local>
2024-06-27 20:20:22 -0300

    conteudo da aula

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SqrtPipe } from './pipes/sqrt.pipe';
+import { DestaquePipe } from './pipes/destaque.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, SqrtPipe, DestaquePipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'testepipe';
+  hoje = new Date();
+  salarioProfessor = 6453.67;
 }
